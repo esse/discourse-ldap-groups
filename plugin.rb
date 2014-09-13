b1dc3d6d4f90c0a22afeec29326652eefbcf0bd3
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-09-14 00:14:30 +0200

    Create categories for groups

--- plugin.rb
+++ plugin.rb
@@ -24,8 +24,15 @@ module ::LdapGroups
     base_dn = SiteSetting.ldap_groups_base_dn
     filter = Net::LDAP::Filter.eq('objectclass', 'posixGroup')
 
+    ldap_group_names = Array.new
     ldap.search(:base => base_dn, :filter => filter) do |entry|
       LdapGroups.update_from_ldap_entry entry
+      ldap_group_names << entry.cn.first
+    end
+    orphaned_groups = GroupCustomField.where(name: 'external_id')
+                                      .where.not(value: ldap_group_names)
+    orphaned_groups.each do |f|
+      delete_group f.group
     end
   end
 
@@ -42,19 +49,65 @@ module ::LdapGroups
     external_name = entry.cn.first
     field = GroupCustomField.find_by(name: 'external_id', 
                                      value: external_name)
-    if field
+    if field and field.group
       group = field.group
     else
-      group = Group.new name: UserNameSuggester.suggest(external_name)
+      name = UserNameSuggester.suggest(external_name)
+      puts "ldap_group: Creating new group '#{name}' for external '#{name}'"
+
+      group = Group.new name: name
       group.visible = false
       group.custom_fields['external_id'] = external_name
-      Rails.logger.info 
-        "ldap_group: Created new group '#{group.name}' for external '#{name}'"
+      group.save!
+    end
+    if SiteSetting.ldap_groups_create_categories
+      create_category group, external_name
     end
 
     # Update group members
     group.users = members
     group.save!
+  end
+
+  def self.create_category(group, name)
+    return if group.custom_fields.has_key? 'category_id'
+    
+    cat = Category.new name: name, user: Discourse.system_user
+    cat.color = SiteSetting.ldap_group_category_color
+    if SiteSetting.ldap_group_category_parent_id
+      cat.parent_category = 
+        Category.find SiteSetting.ldap_group_category_parent_id
+    end
+    cat.set_permissions(group => :full)
+    cat.save!
+
+    category_definition = cat.topics.first
+    category_definition.title = "Private Kategorie der Gruppe #{name}"
+    category_definition.save!
+
+    post = category_definition.posts.first
+    post.revise Discourse.system_user, <<-TEXT.strip_heredoc
+      Dies ist die private Kategorie der Mafiasi-Gruppe *#{name}*.
+      Nur Mitglieder dieser Gruppe können hier mitlesen und schreiben.
+    TEXT
+    post.save!
+
+    group.custom_fields['category_id'] = cat.id
+    group.save!
+  end
+
+  def self.delete_group(group)
+    puts "ldap_group: Deleting '#{group.name}'"
+
+    if group.custom_fields.has_key? 'category_id'
+      # Hide category but do not delete it, in case it has to be recovered
+      cat = Category.find group.custom_fields['category_id']
+      cat.set_permissions(:admins => :readonly)
+      cat.parent_category = Category.find(
+        SiteSetting.ldap_group_deleted_category_parent_id)
+      cat.save!
+    end
+    group.destroy
   end
 end
 
@@ -64,6 +117,7 @@ after_initialize do
       every 1.day
 
       def execute(args)
+        return unless SiteSetting.ldap_groups_enabled
         LdapGroups.update_groups!
       end
     end
